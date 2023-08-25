d9fc12278454ef5aa86d2003554746bf894fb4d6
Erison Cavalcante <erison7596e@gmail.com>
2023-08-25 14:29:48 -0300

    feito animação de envio do excel

--- src/app/upload/page.jsx
+++ src/app/upload/page.jsx
@@ -1,8 +1,7 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Swal from "sweetalert2";
-
 //Icones
 import IconeUp from "@assets/upload.svg";
 
@@ -13,23 +12,25 @@ import api from "@/services/api";
 
 export default function Upload() {
   const [arquivo, setArquivo] = useState(null);
+  const [loading, setLoading] = useState(false); // Estado de carregamento
+
   const atualizarArq = (event) => {
     setArquivo(event.target.files[0]);
   };
 
   const selecionarArq = async () => {
     if (!arquivo) {
-      console.log("Nenhum arquivo selecionado.");
       return;
     }
+
+    setLoading(true); // Ativar o estado de loading
 
     const formData = new FormData();
     formData.append("excelData", arquivo);
 
-    // Enviar o arquivo para o backend
     try {
       const response = await api.post("/importar", formData);
-      // Verificar se o arquivo foi enviado com sucesso
+
       if (response.status === 200) {
         Swal.fire({
           position: "center",
@@ -39,6 +40,16 @@ export default function Upload() {
           timer: 3000,
         });
         setArquivo(null);
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        Swal.fire({
+          position: "center",
+          icon: "error",
+          title: error.response.data, // Mensagem de erro do servidor
+          showConfirmButton: false,
+          timer: 3000,
+        });
       } else {
         Swal.fire({
           position: "center",
@@ -48,9 +59,9 @@ export default function Upload() {
           timer: 3000,
         });
       }
-    } catch (error) {
-      console.log("Erro ao enviar o arquivo:", error);
     }
+
+    setLoading(false); // Desativar o estado de loading após a requisição
   };
 
   return (
@@ -71,18 +82,22 @@ export default function Upload() {
             <div className="flex flex-col items-center m-20 h-3/4 bg-white">
               <span className="font-bold text-3xl mt-16">Importar dados</span>
               <div className="flex flex-col space-y-7 mt-14 items-center justify-center border border-gray-400 w-1/2 h-1/2">
-                <Image src={IconeUp} alt="Upload Icone" />
+                <Image src={IconeUp} />
                 <p className="text-sm">
-                  Selecione um arquivo XLSX, CSV, ODS ou TSV 
+                  Selecione um arquivo XLSX, CSV, ODS ou TSV tamanho do arquivo
+                  não superior a 10 MB
                 </p>
                 {/* Endpoint para envio do arquivo */}
                 <div className="flex flex-col items-center">
                   <input type="file" onChange={atualizarArq} />
                   <button
                     onClick={selecionarArq}
-                    className="mt-8 w-32 border-2 border-blue-m200 rounded-lg px-3 py-2 hover:bg-gradient-to-r from-blue-m100 to-blue-m200 hover:text-white"
+                    disabled={loading} // Desativar o botão enquanto estiver carregando
+                    className={`mt-8 w-32 border-2 border-blue-m200 rounded-lg px-3 py-2 hover:bg-gradient-to-r from-blue-m100 to-blue-m200 hover:text-white ${
+                      loading ? "opacity-50 cursor-not-allowed" : ""
+                    }`}
                   >
-                    Enviar
+                    {loading ? "Enviando..." : "Enviar"}
                   </button>
                 </div>
               </div>
