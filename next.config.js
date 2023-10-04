a4411e6aa3bf1f604dc41a867d195789a866d9c4
Erimilson Silva <eesr@discente.ifpe.edu.br>
2023-10-04 10:31:56 -0300

    feat(rotas) alterando as rotas do back e front

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/app',
+        destination: '/page.jsx',
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
--- src/services/api.jsx
+++ src/services/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // Rota da API do backend
-  baseURL: "http://localhost:4000",
+  baseURL: "http://paineldecustosback.azurewebsites.net",
 });
 
 export default api;
